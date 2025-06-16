45f61117b09e29c3cb07837d0998253da89606f2
user27182 <89109579+user27182@users.noreply.github.com>
2025-06-15 23:52:41 -0600

    Refactor

--- pytest_pyvista/pytest_pyvista.py
+++ pytest_pyvista/pytest_pyvista.py
@@ -2,21 +2,16 @@
 
 from __future__ import annotations
 
-from enum import Enum
 import os
 from pathlib import Path
 import platform
 import shutil
-import sys
-from typing import TYPE_CHECKING
-from typing import NamedTuple
+from typing import Literal
+from typing import cast
 import warnings
 
 import pytest
 import pyvista
-
-if TYPE_CHECKING:  # pragma: no cover
-    from collections.abc import Generator
 
 
 class RegressionError(RuntimeError):
@@ -25,15 +20,6 @@ class RegressionError(RuntimeError):
 
 class RegressionFileNotFound(FileNotFoundError):  # noqa: N818
     """Error when regression file is not found."""
-
-
-class Outcome(Enum):
-    """Outcome of the image verification."""
-
-    SUCCESS = "success"
-    SKIPPED = "skipped"
-    ERROR = "error"
-    WARNING = "warning"
 
 
 def pytest_addoption(parser) -> None:  # noqa: ANN001
@@ -89,11 +75,6 @@ def pytest_addoption(parser) -> None:  # noqa: ANN001
         "--reset_only_failed",
         action="store_true",
         help="Reset only the failed images in the PyVista cache.",
-    )
-    group.addoption(
-        "--fail_unused_cache",
-        action="store_true",
-        help="Report test failure if there are any images in the cache which are not compared to any generated images.",
     )
 
 
@@ -166,6 +147,7 @@ class VerifyImageCache:
         var_error_value=1000.0,  # noqa: ANN001
         var_warning_value=1000.0,  # noqa: ANN001
         generated_image_dir=None,  # noqa: ANN001
+        failed_image_dir=None,  # noqa: ANN001
     ) -> None:
         self.test_name = test_name
 
@@ -183,6 +165,7 @@ class VerifyImageCache:
         self.generated_image_dir = generated_image_dir
         if self.generated_image_dir is not None:
             _ensure_dir_exists(self.generated_image_dir, msg_name="generated image dir")
+        self.failed_image_dir = failed_image_dir
 
         self.high_variance_test = False
         self.windows_skip_image_cache = False
@@ -191,7 +174,7 @@ class VerifyImageCache:
         self.skip = False
         self.n_calls = 0
 
-    def __call__(self, plotter):  # noqa: ANN001, ANN204
+    def __call__(self, plotter):  # noqa: ANN001, ANN204, C901, PLR0912
         """
         Either store or validate an image.
 
@@ -201,6 +184,12 @@ class VerifyImageCache:
             The Plotter object that is being closed.
 
         """
+        if self.skip:
+            return
+
+        if self.ignore_image_cache:
+            return
+
         test_name = f"{self.test_name}_{self.n_calls}" if self.n_calls > 0 else self.test_name
         self.n_calls += 1
 
@@ -211,23 +200,17 @@ class VerifyImageCache:
             allowed_error = self.error_value
             allowed_warning = self.warning_value
 
+        # some tests fail when on Windows with OSMesa
+        if os.name == "nt" and self.windows_skip_image_cache:
+            return
+        # high variation for MacOS
+        if platform.system() == "Darwin" and self.macos_skip_image_cache:
+            return
+
         # cached image name. We remove the first 5 characters of the function name
         # "test_" to get the name for the image.
-        image_name = _image_name_from_test_name(test_name)
+        image_name = test_name[5:] + ".png"
         image_filename = os.path.join(self.cache_dir, image_name)  # noqa: PTH118
-        gen_image_filename = None
-
-        skip_windows = os.name == "nt" and self.windows_skip_image_cache
-        skip_macos = platform.system() == "Darwin" and self.macos_skip_image_cache
-        if self.skip or self.ignore_image_cache or skip_windows or skip_macos:
-            # Log result as skipped
-            _store_result(
-                test_name=test_name,
-                outcome=Outcome.SKIPPED,
-                cached_filename=image_name,
-                generated_filename=gen_image_filename,
-            )
-            return
 
         if not os.path.isfile(image_filename) and self.fail_extra_image_cache and not self.reset_image_cache:  # noqa: PTH113
             # Make sure this doesn't get called again if this plotter doesn't close properly
@@ -245,7 +228,8 @@ class VerifyImageCache:
         error = pyvista.compare_images(image_filename, plotter)
 
         if error > allowed_error:
-            _store_result(test_name=test_name, outcome=Outcome.ERROR, cached_filename=image_filename, generated_filename=gen_image_filename)
+            if self.failed_image_dir is not None:
+                self._save_failed_test_images("error", plotter, image_name)
             if self.reset_only_failed:
                 warnings.warn(  # noqa: B028
                     f"{test_name} Exceeded image regression error of "
@@ -259,10 +243,30 @@ class VerifyImageCache:
                 msg = f"{test_name} Exceeded image regression error of {allowed_error} with an image error equal to: {error}"
                 raise RegressionError(msg)
         if error > allowed_warning:
-            _store_result(test_name=test_name, outcome=Outcome.WARNING, cached_filename=image_filename, generated_filename=gen_image_filename)
+            if self.failed_image_dir is not None:
+                self._save_failed_test_images("warning", plotter, image_name)
             warnings.warn(f"{test_name} Exceeded image regression warning of {allowed_warning} with an image error of {error}")  # noqa: B028
-        else:
-            _store_result(test_name=test_name, outcome=Outcome.SUCCESS, cached_filename=image_filename, generated_filename=gen_image_filename)
+
+    def _save_failed_test_images(self, error_or_warning: Literal["error", "warning"], plotter: pyvista.Plotter, image_name: str) -> None:
+        def _make_failed_test_image_dir(
+            errors_or_warnings: Literal["errors", "warnings"], from_cache_or_test: Literal["from_cache", "from_test"]
+        ) -> Path:
+            """Save test image from cache or test to the failed image dir."""
+            _ensure_dir_exists(self.failed_image_dir, msg_name="failed image dir")
+            Path(self.failed_image_dir, errors_or_warnings).mkdir(exist_ok=True)
+
+            dest_dir = Path(self.failed_image_dir, errors_or_warnings, from_cache_or_test)
+            dest_dir.mkdir(exist_ok=True)
+            return dest_dir
+
+        error_dirname = cast("Literal['errors', 'warnings']", error_or_warning + "s")
+        from_test_dir = _make_failed_test_image_dir(error_dirname, "from_test")
+        plotter.screenshot(Path(from_test_dir, image_name))
+
+        cached_image = Path(self.cache_dir, image_name)
+        if cached_image.is_file():
+            from_cache_dir = _make_failed_test_image_dir(error_dirname, "from_cache")
+            shutil.copy(cached_image, Path(from_cache_dir, image_name))
 
 
 def _ensure_dir_exists(dirpath: str, msg_name: str) -> None:
@@ -279,124 +283,6 @@ def _get_dir_from_config_or_ini(pytestconfig, dirname: str) -> str:  # noqa: ANN
     return gen_dir
 
 
-def _image_name_from_test_name(test_name: str) -> str:
-    return test_name[5:] + ".png"
-
-
-def _test_name_from_image_name(image_name: str) -> str:
-    def remove_suffix(s: str) -> str:
-        """Remove integer and png suffix."""
-        no_png_ext = s[:-4]
-        parts = no_png_ext.split("_")
-        if len(parts) > 1:
-            try:
-                int(parts[-1])
-                parts = parts[:-1]  # Remove the integer suffix
-            except ValueError:
-                pass  # Last part is not an integer; do nothing
-        return "_".join(parts)
-
-    return "test_" + remove_suffix(image_name)
-
-
-class _ResultTuple(NamedTuple):
-    outcome: Outcome
-    cached_filename: str
-    generated_filename: str | None
-
-
-RESULTS = {}
-
-
-def _store_result(*, test_name: str, outcome: Outcome, cached_filename: str, generated_filename: str | None = None) -> None:
-    result = _ResultTuple(
-        outcome=outcome,
-        cached_filename=str(Path(cached_filename).name),
-        generated_filename=str(Path(generated_filename).name) if generated_filename else None,
-    )
-    RESULTS[test_name] = result
-
-
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call) -> Generator:  # noqa: ANN001, ARG001
-    """Store test results for skipped tests."""
-    outcome = yield
-    if outcome:
-        rep = outcome.get_result()
-
-        # Log if test was skipped
-        if rep.when in ["call", "setup"] and rep.skipped:
-            test_name = item.name
-            _store_result(
-                test_name=test_name,
-                outcome=Outcome.SKIPPED,
-                cached_filename=_image_name_from_test_name(test_name),
-                generated_filename=None,
-            )
-
-
-@pytest.hookimpl
-def pytest_sessionfinish(session, exitstatus) -> None:  # noqa: ANN001, ARG001
-    """Execute after the whole test run completes."""
-    config = session.config
-
-    fail_unused_cache = config.getoption("fail_unused_cache")
-    image_cache_dir = _get_dir_from_config_or_ini(config, "image_cache_dir")
-    failed_image_dir = _get_dir_from_config_or_ini(config, "failed_image_dir")
-    generated_image_dir = _get_dir_from_config_or_ini(config, "generated_image_dir")
-
-    if image_cache_dir and fail_unused_cache:
-        cache_path = Path(image_cache_dir)
-        cached_files = {f.name for f in cache_path.glob("*.png")}
-        tested_files = {result.cached_filename for result in RESULTS.values()}
-        unused = cached_files - tested_files
-
-        # Exclude images from skipped tests where multiple images are generated
-        unused_skipped = unused.copy()
-        for image_name in unused:
-            test_name = _test_name_from_image_name(image_name)
-            result = RESULTS.get(test_name)
-            if result and result.outcome == Outcome.SKIPPED:
-                unused_skipped.remove(image_name)
-
-        if unused_skipped:
-            msg = (
-                f"\npytest-pyvista: ERROR: Unused cached image file(s) detected ({len(unused_skipped)}).\n"
-                f"The following images were not generated or skipped by any of the tests:\n"
-                f"{sorted(unused_skipped)}\n"
-            )
-            # Print the message so it appears in the output
-            sys.stderr.write(msg)
-            sys.stderr.flush()
-
-            session.exitstatus = pytest.ExitCode.TESTS_FAILED
-
-    if failed_image_dir:
-        for result in RESULTS.values():
-            if result.outcome in [Outcome.WARNING, Outcome.ERROR]:
-                cached_image_path = Path(image_cache_dir, result.cached_filename)
-                if cached_image_path.is_file():
-                    _ensure_dir_exists(failed_image_dir, msg_name="failed image dir")
-                    _save_failed_test_image(cached_image_path, result.outcome, image_cache_dir, failed_image_dir)
-                if result.generated_filename:
-                    _ensure_dir_exists(failed_image_dir, msg_name="failed image dir")
-                    generated_image_path = Path(generated_image_dir, result.generated_filename)
-                    _save_failed_test_image(generated_image_path, result.outcome, image_cache_dir, failed_image_dir)
-
-    RESULTS.clear()
-
-
-def _save_failed_test_image(source_image_path: Path, outcome: Outcome, image_cache_dir: str, failed_image_dir: str) -> None:
-    """Save test image from cache or test to the failed image dir."""
-    parent_dir = Path(outcome.name.lower() + "s")
-    dest_dirname = "from_cache" if Path(source_image_path).parent == Path(image_cache_dir) else "from_test"
-    Path(failed_image_dir, parent_dir).mkdir(exist_ok=True)
-    dest_dir = Path(failed_image_dir, parent_dir, dest_dirname)
-    dest_dir.mkdir(exist_ok=True)
-    dest_path = Path(dest_dir, Path(source_image_path).name)
-    shutil.copy(source_image_path, dest_path)
-
-
 @pytest.fixture
 def verify_image_cache(request, pytestconfig):  # noqa: ANN001, ANN201
     """Checks cached images against test images for PyVista."""  # noqa: D401
@@ -409,8 +295,9 @@ def verify_image_cache(request, pytestconfig):  # noqa: ANN001, ANN201
 
     cache_dir = _get_dir_from_config_or_ini(pytestconfig, "image_cache_dir")
     gen_dir = _get_dir_from_config_or_ini(pytestconfig, "generated_image_dir")
+    failed_dir = _get_dir_from_config_or_ini(pytestconfig, "failed_image_dir")
 
-    verify_image_cache = VerifyImageCache(request.node.name, cache_dir, generated_image_dir=gen_dir)
+    verify_image_cache = VerifyImageCache(request.node.name, cache_dir, generated_image_dir=gen_dir, failed_image_dir=failed_dir)
     pyvista.global_theme.before_close_callback = verify_image_cache
 
     def reset() -> None:
