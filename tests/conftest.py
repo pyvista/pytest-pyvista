040a1e3b39d155602a6396556bc2f2d1d9618926
user27182 <89109579+user27182@users.noreply.github.com>
2025-09-08 12:54:13 -0600

    Auto patch system properties

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,14 @@
 from __future__ import annotations  # noqa: D100
 
+import pytest
+
+from pytest_pyvista.pytest_pyvista import _SystemProperties
+
 pytest_plugins = "pytester"
+
+
+@pytest.fixture(autouse=True)
+def reset_system_properties(monkeypatch) -> None:
+    """Reset system properties."""
+    # Replace the module-level _SYSTEM_PROPERTIES with a fresh instance
+    monkeypatch.setattr("pytest_pyvista.pytest_pyvista._SYSTEM_PROPERTIES", _SystemProperties())
