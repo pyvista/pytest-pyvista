be7a06ed8e389cda8d7cc48d461f509d7fca5903
user27182 <89109579+user27182@users.noreply.github.com>
2025-09-09 12:17:48 -0600

    Close all

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,12 @@
 from __future__ import annotations  # noqa: D100
 
+import pytest
+import pyvista as pv
+
 pytest_plugins = "pytester"
+
+
+@pytest.fixture(autouse=True)
+def close_all_plotters() -> None:
+    """Close all."""
+    pv.close_all()
