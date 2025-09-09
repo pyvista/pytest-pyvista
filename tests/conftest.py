64e651a36f4fd37d1baad6d9067337f8c94e0443
user27182 <89109579+user27182@users.noreply.github.com>
2025-09-09 12:29:06 -0600

    Yield in fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,18 @@
 from __future__ import annotations  # noqa: D100
 
+from typing import TYPE_CHECKING
+
 import pytest
 import pyvista as pv
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
 
 pytest_plugins = "pytester"
 
 
 @pytest.fixture(autouse=True)
-def close_all_plotters() -> None:
+def close_all_plotters() -> Generator[None]:
     """Close all."""
+    yield
     pv.close_all()
